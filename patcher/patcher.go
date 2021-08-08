91db998323b48ed9857530e41cec735c190cb5e0
benjamin-schultz <41674946+benjamin-schultz@users.noreply.github.com>
2021-08-08 19:36:43 +1000

    Move to new function
    Corrected a few of the comments:
    - Moved to independent function
    - fixed type issues
    - Changed order of API offset checking

--- patcher/patcher.go
+++ patcher/patcher.go
@@ -26,12 +26,90 @@ var procGetModuleFileNameExA *windows.LazyProc = modPSAPI.NewProc("GetModuleFile
 // Errors
 var ErrProcessAlreadyPatched = errors.New("process already patched")
 var ErrProcessNotFound = errors.New("couldn't find process")
+var ErrAPINotFound = errors.New("couldn't find API address in memory")
 
 func min(a uint32, b uint32) uint32 {
 	if a > b {
 		return b
 	}
 	return a
+}
+
+func SearchMemory(proc windows.Handle, LPBaseOfDll uintptr, SizeOfImage uint32, value []byte, altvalue []byte) (uintptr, error) {
+	// Information about the contents of the memory to read
+	var memoryBasicInfo struct {
+		BaseAddress       uintptr
+		AllocationBase    uintptr
+		AllocationProtect uint32
+		PartitionId       uint16
+		RegionSize        uintptr
+		State             uint32
+		Protect           uint32
+		Type              uint32
+	}
+
+	var memoryBasicInfoSize = uint32(unsafe.Sizeof(memoryBasicInfo))
+
+	var p uintptr = LPBaseOfDll
+	var offset uintptr
+
+	// Programmatically find the offset of the API url
+	// Don't search beyond the end of the application memory
+	for p < LPBaseOfDll+uintptr(SizeOfImage) {
+		ret, _, err := procVirtualQueryEx.Call(uintptr(proc), p, uintptr(unsafe.Pointer(&memoryBasicInfo)), uintptr(memoryBasicInfoSize))
+		if ret == 0 {
+			return 0, fmt.Errorf("error in VirtualQueryEx: %w", err)
+		} else if ret == unsafe.Sizeof(memoryBasicInfo) {
+			var bytesRead uint32
+			var chunk = make([]byte, memoryBasicInfo.RegionSize)
+			// Read the chunk of memory into a byte array
+			ret, _, err = procReadProcessMemory.Call(uintptr(proc), memoryBasicInfo.BaseAddress, uintptr(unsafe.Pointer(&chunk[0])), uintptr(memoryBasicInfo.RegionSize), uintptr(unsafe.Pointer(&bytesRead)))
+			if ret == 0 {
+				return 0, fmt.Errorf("error in ReadProcessMemory: %w", err)
+			} else if ret != 0 {
+				// See if the chunk contains the API url and get its offset if its there
+				// Only gets the first instance of the API url in memory
+				var ind = bytes.Index(chunk[:bytesRead], value)
+				if ind != -1 {
+					// Override offset with the found API url index
+					offset = p + uintptr(ind)
+					return offset, nil
+				}
+				// If the chunk doesn't have the API address, check if its already been patched?
+				ind = bytes.Index(chunk[:bytesRead], altvalue)
+				if ind != -1 {
+					offset = p + uintptr(ind)
+					return offset, ErrProcessAlreadyPatched
+				}
+
+			}
+			// If not found in this chunk, try the next chunk
+			p = p + uintptr(memoryBasicInfo.RegionSize)
+		}
+	}
+	// If can't find api url in memory, then use the predefined offset and hope its there
+
+	return 0, ErrAPINotFound
+}
+
+func VerifyAPIOffset(proc windows.Handle, offset uintptr, old []byte, new []byte) (uintptr, error) {
+	var buf = make([]byte, len(old))
+	var bytesRead uint32
+
+	// Verify we're at the correct offset
+	ret, _, err := procReadProcessMemory.Call(uintptr(proc), offset, uintptr(unsafe.Pointer(&buf[0])), uintptr(len(old)), uintptr(unsafe.Pointer(&bytesRead)))
+	if ret == 0 { // err is always set, even on success. Need to look at return value
+		return offset, fmt.Errorf("error in ReadProcessMemory: %w", err)
+	}
+
+	if !bytes.Equal(buf[:bytesRead], old) {
+		if bytes.Equal(buf[:min(bytesRead, uint32(len(new)))], new) {
+			return offset, ErrProcessAlreadyPatched
+		}
+		return offset, fmt.Errorf("%q does not match signature at offset 0x%x", buf[:bytesRead], offset)
+	}
+
+	return offset, nil
 }
 
 func GetProc(proc string) (uint32, error) {
@@ -116,65 +194,14 @@ func PatchProc(pid uint32, moduleName string, offsetAddr uintptr, old []byte, ne
 
 	var offset = moduleInfo.LPBaseOfDll + offsetAddr
 
-	// Information about the contents of the memory to read
-	var memoryBasicInfo struct {
-		BaseAddress       uintptr
-		AllocationBase    uintptr
-		AllocationProtect uint32
-		PartitionId       uint32
-		RegionSize        uint64
-		State             uint32
-		Protect           uint32
-		Type              uint32
-	}
-
-	var memoryBasicInfoSize = uint32(unsafe.Sizeof(memoryBasicInfo))
-
-	var p uintptr = moduleInfo.LPBaseOfDll
-
-	// Programmatically find the offset of the API url
-	// Don't search beyond the end of the application memory
-	for p < moduleInfo.LPBaseOfDll+uintptr(moduleInfo.SizeOfImage) {
-		ret, _, err = procVirtualQueryEx.Call(uintptr(proc), p, uintptr(unsafe.Pointer(&memoryBasicInfo)), uintptr(memoryBasicInfoSize))
-		if ret == 0 {
-			return 0, fmt.Errorf("error in VirtualQueryEx: %w", err)
-		} else if ret == unsafe.Sizeof(memoryBasicInfo) {
-			var bytesRead uint32
-			var chunk = make([]byte, memoryBasicInfo.RegionSize)
-			// Read the chunk of memory into a byte array
-			ret, _, err = procReadProcessMemory.Call(uintptr(proc), memoryBasicInfo.BaseAddress, uintptr(unsafe.Pointer(&chunk[0])), uintptr(memoryBasicInfo.RegionSize), uintptr(unsafe.Pointer(&bytesRead)))
-			if ret == 0 {
-				return 0, fmt.Errorf("error in ReadProcessMemory: %w", err)
-			} else if ret != 0 {
-				// See if the chunk contains the API url and get its offset if its there
-				// Only gets the first instance of the API url in memory
-				var ind = bytes.Index(chunk[:bytesRead], []byte(old))
-				if ind != -1 {
-					// Override offset with the found API url index
-					offset = p + uintptr(ind)
-					break
-				}
-			}
-			// If not found in this chunk, try the next chunk
-			p = p + uintptr(memoryBasicInfo.RegionSize)
+	// Check if the API is at the offset specified
+	offset, err = VerifyAPIOffset(proc, offset, old, new)
+	if err != nil {
+		// If the offset doesn't have the old or new API address, try searching memory
+		offset, err = SearchMemory(proc, moduleInfo.LPBaseOfDll, moduleInfo.SizeOfImage, old, new)
+		if err != nil {
+			return offset, err
 		}
-	}
-	// If can't find api url in memory, then use the predefined offset and hope its there
-
-	var buf = make([]byte, len(old))
-	var bytesRead uint32
-
-	// Verify we're at the correct offset
-	ret, _, err = procReadProcessMemory.Call(uintptr(proc), offset, uintptr(unsafe.Pointer(&buf[0])), uintptr(len(old)), uintptr(unsafe.Pointer(&bytesRead)))
-	if ret == 0 { // err is always set, even on success. Need to look at return value
-		return offset, fmt.Errorf("error in ReadProcessMemory: %w", err)
-	}
-
-	if !bytes.Equal(buf[:bytesRead], old) {
-		if bytes.Equal(buf[:min(bytesRead, uint32(len(new)))], new) {
-			return offset, ErrProcessAlreadyPatched
-		}
-		return offset, fmt.Errorf("%q does not match signature at offset 0x%x", buf[:bytesRead], offset)
 	}
 
 	// Set memory writable
@@ -185,7 +212,7 @@ func PatchProc(pid uint32, moduleName string, offsetAddr uintptr, old []byte, ne
 	}
 
 	var bytesWritten uint32
-	buf = make([]byte, len(old))
+	buf := make([]byte, len(old))
 	copy(buf, new)
 	ret, _, err = procWriteProcessMemory.Call(uintptr(proc), offset, uintptr(unsafe.Pointer(&buf[0])), uintptr(len(old)), uintptr(unsafe.Pointer(&bytesWritten)))
 	if ret == 0 { // err is always set, even on success. Need to look at return value
